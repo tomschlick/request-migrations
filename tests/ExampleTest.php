3ef298021297285730e02ae07fdccf2545da0ad9
Tom Schlick <tom@tomschlick.com>
2017-08-15 22:39:34 -0400

    testsuite
    fixes #2

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TomSchlick\RequestMigrations\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
