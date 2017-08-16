58d094d78373670a6c469fa3c799c1d8b2e001f8
Tom Schlick <tom@tomschlick.com>
2017-08-15 22:40:23 -0400

    service provider
    fixes #6

---
+++ src/RequestMigrationsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TomSchlick\RequestMigrations;
+
+use Illuminate\Support\ServiceProvider;
+
+class RequestMigrationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('request-migrations.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'request-migrations');
+    }
+}
