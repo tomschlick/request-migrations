fc97737ae81f1cc66bd25a1c9cf371bb225cef3d
Tom Schlick <tom@tomschlick.com>
2017-08-18 00:53:59 -0400

    new test class

---
+++ tests/MigratorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TomSchlick\RequestMigrations\Tests;
+
+class MigratorTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function it_will_only_get_versions_behind_current()
+    {
+        //
+    }
+}
