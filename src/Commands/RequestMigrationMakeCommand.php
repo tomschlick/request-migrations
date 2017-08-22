c705a667ba6b9516ab2a5a6c05778d6fb36228df
sixlive <tj@tjmiller.co>
2017-08-22 16:26:14 -0400

    Adds make command for request migrations

---
+++ src/Commands/RequestMigrationMakeCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace TomSchlick\RequestMigrations\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class RequestMigrationMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:request-migration';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new request migration';
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool|null
+     */
+    public function handle()
+    {
+        parent::handle();
+    }
+    
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/../../stubs/migration.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Http\Requests\Migrations';
+    }
+}
