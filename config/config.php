547b8ad0620461b9b95798bd679a91057135058d
Luke Policinski <luke@lukepolo.com>
2017-08-30 14:14:57 -0400

    adding some headers in the config

--- config/config.php
+++ config/config.php
@@ -2,11 +2,29 @@
 
 return [
 
-    'headers'         => [
+    /*
+    |--------------------------------------------------------------------------
+    | Headers
+    |--------------------------------------------------------------------------
+    |
+    | You can customize your headers that you want to send in with each request
+    |
+    */
+
+    'headers' => [
         'current-version'  => 'x-api-current-version',
         'request-version'  => 'x-api-request-version',
         'response-version' => 'x-api-response-version',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Current Version
+    |--------------------------------------------------------------------------
+    |
+    | This is the version users will be defaulted to
+    |
+    */
 
     'current_version' => '',
 
