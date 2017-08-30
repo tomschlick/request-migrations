5581b8a8fd1334d8c6250847f516c2a148ee5d9b
Luke Policinski <luke@lukepolo.com>
2017-08-30 11:14:11 -0400

    some validation for the date

--- src/Commands/RequestMigrationMakeCommand.php
+++ src/Commands/RequestMigrationMakeCommand.php
@@ -46,6 +46,11 @@ class RequestMigrationMakeCommand extends GeneratorCommand
             $this->info('Please set your default version in your request-migrations config');
         }
 
+
+        if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->version)) {
+            return $this->error('You provided a invalid date');
+        }
+
         parent::handle();
     }
 
